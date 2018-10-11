68919d6cba776a169b993e09eca6d4f65db023af
Liam Doran <liamfd@gmail.com>
2018-10-11 19:28:17 -0400

    test: ~log a success message in green~ wrote jest

--- src/test.js
+++ src/test.js
@@ -5,3 +5,7 @@ const port = 5050;
 // just make sure we can spin and shut down a server
 start({ port, inMemory: true });
 stop(port);
+
+const fgGreen = '\x1b[32m';
+const resetColor = '\x1b[0m';
+console.info(`${fgGreen}%s${resetColor}`, 'Test passed! You are doing great.');
